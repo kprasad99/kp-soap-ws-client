73d5408ef061f9ff69e68394135f7d7a4d25ebb6
karthik.p <karthik.p@altencalsoftlabs.com>
2017-08-20 00:10:09 +0530

    Moved hard-coded server url to property file.

--- src/main/java/com/kp/swasthik/soap/CxfConfig.java
+++ src/main/java/com/kp/swasthik/soap/CxfConfig.java
@@ -5,19 +5,27 @@ import java.util.Collections;
 import org.apache.cxf.bus.spring.SpringBus;
 import org.apache.cxf.feature.LoggingFeature;
 import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.kp.swasthik.ws.HelloWorld;
 
 @Configuration
+@ConfigurationProperties(prefix = "cxf")
 public class CxfConfig {
+
+	private String serverUrl;
+
+	public void setServerUrl(String serverUrl) {
+		this.serverUrl = serverUrl;
+	}
 
 	@Bean
 	public HelloWorld bus(SpringBus bus) {
 		JaxWsProxyFactoryBean bean = new JaxWsProxyFactoryBean();
 		bean.setServiceClass(HelloWorld.class);
-		bean.setAddress("http://localhost:8484/services/kp?wsdl");
+		bean.setAddress(serverUrl);
 		bean.setBus(bus);
 		bean.setFeatures(Collections.singletonList(new LoggingFeature()));
 		return bean.create(HelloWorld.class);
